db3125c7eb1c8c21ab46f735b5854cda507dc55d
patrick-steve <75305373+patrick-steve@users.noreply.github.com>
2023-03-18 16:22:45 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASkoAvLIjavqI-EQNcF4jeU0_3xn6WIyw",
-  authDomain: "todolist-e6311.firebaseapp.com",
-  databaseURL: "https://todolist-e6311-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "todolist-e6311",
-  storageBucket: "todolist-e6311.appspot.com",
-  messagingSenderId: "640698028294",
-  appId: "1:640698028294:web:d05b389c31d5635d040e87",
-  measurementId: "G-9VFD58MB81"
+  apiKey: "***",
+  authDomain: "**",
+  databaseURL: "**",
+  projectId: "***",
+  storageBucket: "**",
+  messagingSenderId: "****",
+  appId: "*",
+  measurementId: "***"
 };
 
 const app = initializeApp(firebaseConfig);
